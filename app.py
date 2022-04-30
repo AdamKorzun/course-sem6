8ac3c2263f3e3410c76970ad49a73cb6e1e182a5
AdamKorzun <a.korzun21@gmail.com>
2022-05-01 01:20:15 +0300

    file upload

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request,  render_template,  redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from models import *
+from flask_dropzone import Dropzone
+import os
 
 app = Flask(__name__)
+dropzone = Dropzone(app)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -15,11 +18,18 @@ def scan():
 @app.route('/add/', methods=['POST', 'GET'])
 def add_virus():
     return render_template('add.html')
-    
+
 @app.route('/history/', methods=['GET'])
 def history():
     return render_template('history.html')
 
+@app.route('/file_upload/', methods=['GET', 'POST'])
+def file_upload():
+    if request.method == 'POST':
+        f = request.files.get('file')
+        f.save(os.path.join('upload_files', f.filename))
+
+    return 'upload template'
 if __name__ == '__main__':
     app.secret_key = 'super_secret'
     app.run(host='127.0.0.1', port='5000', debug=True)
--- static/scripts/input_path_control.js
+++ static/scripts/input_path_control.js
@@ -3,11 +3,12 @@ window.onload = function () {
   var path = document.getElementById('path-input')
   var btn_file = document.getElementById('control_02')
   var btn_directory = document.getElementById('control_03')
-
+  var dz = document.getElementById('myDropzone')
   path.style.visibility = "hidden";
   btn.addEventListener("click", () => {
     if (this.checked) {
       path.style.visibility = "visible";
+      dz.style.visibility = "hidden";
     }
     else {
       path.style.visibility = "hidden";
@@ -16,20 +17,24 @@ window.onload = function () {
   btn_file.addEventListener("click", () => {
     if (this.checked) {
       path.style.visibility = "hidden";
+      dz.style.visibility = "visible";
 
     }
     else {
       path.style.visibility = "visible";
+      dz.style.visibility = "hidden";
 
     }
   });
   btn_directory.addEventListener("click", () => {
     if (this.checked) {
       path.style.visibility = "hidden";
+      dz.style.visibility = "visible";
 
     }
     else {
       path.style.visibility = "visible";
+      dz.style.visibility = "hidden";
 
     }
   });
